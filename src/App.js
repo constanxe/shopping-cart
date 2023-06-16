a0f868dac57587601e00f89f2e59b64a49c5a052
Constance <54532144+constanxe@users.noreply.github.com>
2023-06-17 02:37:33 +0800

    static products -  use `Section` component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Section header="Products available 🛍"></Section>
       <Section header="Your shopping cart 🛒"></Section>
     </div>
   );
