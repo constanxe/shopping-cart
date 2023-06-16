793524ac2a2f47bd38c8c4b200ea8b83835d0407
Constance <54532144+constanxe@users.noreply.github.com>
2023-06-17 02:05:22 +0800

    static cart - section, table

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Your shopping cart 🛒
-      </header>
+      <section>
+        <header className="header">
+          Your shopping cart 🛒
+        </header>
+
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price ($)</th>
+            <th>Action</th>
+          </tr>
+
+          <tr>
+            <td>a</td>
+            <td>2</td>
+            <td>2.00</td>
+            <td><button>Remove</button></td>
+          </tr>
+
+          <tr>
+            <td colSpan={2}><b>Total price ($):</b></td>
+            <td>2.00</td>
+          </tr>
+        </table>
+      </section>
     </div>
   );
 }
