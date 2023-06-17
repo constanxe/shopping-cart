3f59cd1db6efd859773c44eebd68ee2855b73caf
Constance <54532144+constanxe@users.noreply.github.com>
2023-06-18 00:41:53 +0800

    context: cart reducer

--- src/state/CartContext.js
+++ src/state/CartContext.js
@@ -29,6 +29,42 @@ export function useCartDispatch() {
 
 function cartReducer(cart, action) {
   switch (action.type) {
+    case 'add': {
+      return [...cart, {
+        ...action.item,
+        quantity: 0
+      }];
+    }
+    case 'decrease': {
+      return cart.map(item => {
+        if (item.name === action.name) {
+          return {
+            ...item,
+            quantity: item.quantity - 1
+          };
+        } else {
+          return item;
+        }
+      });
+    }
+    case 'increase': {
+      return cart.map(item => {
+        if (item.name === action.name) {
+          return {
+            ...item,
+            quantity: item.quantity - 1
+          };
+        } else {
+          return item;
+        }
+      });
+    }
+    case 'remove': {
+      return cart.filter(item => item.name !== action.name);
+    }
+    case 'clear': {
+      return [];
+    }
     default: {
       throw Error('Unknown action: ' + action.type);
     }
