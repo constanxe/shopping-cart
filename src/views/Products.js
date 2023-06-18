d5de05e57cdc2ba4dcfbcbdbf6b3b7cceb268bbb
Constance <54532144+constanxe@users.noreply.github.com>
2023-06-18 16:26:52 +0800

    views: products - add loading

--- src/views/Products.js
+++ src/views/Products.js
@@ -3,20 +3,27 @@ import { useState, useEffect } from 'react';
 import { Header, Hint, TableItems } from '../components/index';
 
 const Products = () => {
+	const [isLoading, setIsLoading] = useState(true);
 	const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('https://dummyjson.com/products')
+    fetch('https://dummyjson.com/poducts')
       .then((response) => response.json())
       .then((data) => setProducts(data.products))
-      .catch((err) => console.error(err.message));
+			.catch((err) => console.error(err.message))
+			.finally(() => setIsLoading(false));
  }, []);
 
 	return (
 		<div>
 			<Header>Products available 🛍</Header>
-			<TableItems items={products}></TableItems>
-			{!products.length && <Hint></Hint>}
+			{isLoading ?
+				<small>Loading...</small> :
+				(products.length ?
+					<TableItems items={products}></TableItems> :
+					<Hint></Hint>
+				)
+			}
 		</div>
   );
 }
