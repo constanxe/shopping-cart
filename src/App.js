8838463dbde4c79f3749a2438c89157dbb0a9c63
Constance <54532144+constanxe@users.noreply.github.com>
2023-06-18 16:16:01 +0800

    views: add nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Products, Cart, PageNotFound } from './views/index';
 
@@ -8,6 +8,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <nav>
+          <NavLink to="/products">Products</NavLink> |&nbsp;
+          <NavLink to="/cart">Cart</NavLink> |&nbsp;
+          <NavLink to="/does-not-exist"><i>Catch all route</i></NavLink>
+        </nav>
+
         <Routes>
           <Route path="/" element={<Navigate to="/products" />} />
           <Route exact path="/products" element={<Products />} />
